b3fab5daed5f3182ba6ce52092bdd577f10f0843
Carlo Attardi <carloattardi15@gmail.com>
2017-11-04 22:42:10 +0100

    aggiunta view di cgestione

---
+++ hair-future/View/VGestione.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: carlo
+ * Date: 04/11/17
+ * Time: 19.06
+ */
+
+class VGestione extends VJson
+{
+    public function inviaOrario($orario)
+    {
+        $dati = $orario->convertiInArray();
+        $this->invia($dati);
+    }
+
+    /**
+     * riceve un array di array chiave/valore dove le chiavi sono:
+     *
+     * giorno
+     * aperturaMattina
+     * chiusuraMattina
+     * aperturaPomeriggio
+     * chiusuraPomeriggio
+     *
+     * @return mixed
+     */
+    public function riceviOrario()
+    {
+        $dati = $this->ricevi();
+        return $dati['dati']['nuovoOrario'];
+    }
+
+    public function riceviInizioPeriodo()
+    {
+        $dati = $this->ricevi();
+        return $dati['dati']['data'];
+    }
+
+    public function riceviDurataPeriodo()
+    {
+        $dati = $this->ricevi();
+        return $dati['dati']['numeroGiorni'];
+    }
+
+    public function riceviEffettuati()
+    {
+        $dati = $this->ricevi();
+        return $dati['dati']['appuntamentiEffettuati'];
+    }
+}
