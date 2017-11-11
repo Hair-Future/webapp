22dd8bf4e0e428b125e67e2fab8975cc85331836
Carlo Attardi <carloattardi15@gmail.com>
2017-11-11 19:13:40 +0100

    aggiunta la possibilità di segnare un appuntamento come effettuato

--- hair-future/inserimentoAppuntamenti.php
+++ hair-future/inserimentoAppuntamenti.php
@@ -17,6 +17,8 @@ $idUtenti = array("marzia.toscani@hairfuture.it", "veneranda.calabrese@hairfutur
 
 $catalogo = new ECatalogoServizi();
 
+$aBuonFine = 0;
+
 for ($i = 0; $i < 500; $i++)
 {
     $servizi = array();
@@ -27,10 +29,20 @@ for ($i = 0; $i < 500; $i++)
     }
     $rand = rand(0, 7);
     $id = $idUtenti[$rand];
-    var_dump($id);
     $signora = EGestoreUtenti::ottieniUtenteByID($id);
-    var_dump($signora);
 
     $date = new DateTime(date("Y-m-d H:i:s",rand(strtotime('2017-10-01'),strtotime('2017-12-23'))));
-    $signora->prenotaAppuntamento($servizi, $date->format('Y-m-d'), ''.rand(9,19).':00:00');
+    $ret = $signora->prenotaAppuntamento($servizi, $date->format('Y-m-d'), ''.rand(9,19).':00:00');
+    if ($ret == 0)
+    {
+        $aBuonFine++;
+    }
+}
+
+
+$catalogo = new ECatalogoAppuntamenti();
+for ($i=0; $i<500; $i++)
+{
+    $rand = rand(1, $aBuonFine);
+    $catalogo->segnaEffettuato($rand);
 }
